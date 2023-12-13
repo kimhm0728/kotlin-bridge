9e50265d53cd001d3e4b89fba9d516afe9ab51fe
kimhm0728 <kimhm0728@naver.com>
2023-12-13 16:48:59 +0900

    feat: 게임 시작 문구 출력하는 기능 구현

--- src/main/kotlin/bridge/io/OutputView.kt
+++ src/main/kotlin/bridge/io/OutputView.kt
@@ -1,9 +1,12 @@
 package bridge.io
 
-/**
- * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
- */
 class OutputView {
+
+    fun printStart() {
+        println("다리 건너기 게임을 시작합니다.")
+        lineBreak()
+    }
+
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      *
@@ -17,4 +20,6 @@ class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     fun printResult() {}
+
+    private fun lineBreak() = println()
 }
