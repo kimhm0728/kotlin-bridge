6fdd640565fc4d4cf3508397af5cd14d92f934ac
kimhm0728 <kimhm0728@naver.com>
2023-12-13 16:58:47 +0900

    feat: 재시도 여부 예외 처리하는 기능 구현

--- src/main/kotlin/bridge/constants/Constants.kt
+++ src/main/kotlin/bridge/constants/Constants.kt
@@ -2,5 +2,7 @@ package bridge.constants
 
 enum class Constants(val value: Char) {
     MOVING_UP('U'),
-    MOVING_DOWN('D')
+    MOVING_DOWN('D'),
+    GAME_COMMAND_RETRY('R'),
+    GAME_COMMAND_QUIT('Q')
 }
--- src/main/kotlin/bridge/io/input/InputValidator.kt
+++ src/main/kotlin/bridge/io/input/InputValidator.kt
@@ -2,6 +2,8 @@ package bridge.io.input
 
 import bridge.constants.Constants.MOVING_UP
 import bridge.constants.Constants.MOVING_DOWN
+import bridge.constants.Constants.GAME_COMMAND_RETRY
+import bridge.constants.Constants.GAME_COMMAND_QUIT
 
 class InputValidator {
 
@@ -26,6 +28,14 @@ class InputValidator {
 
     private fun Char.validateMoving() =
         require(this == MOVING_UP.value || this == MOVING_DOWN.value) { "하나의 문자만 입력해 주세요." }
+
+    fun validateGameCommand(gameCommand: String) {
+        gameCommand.validateLength()
+        gameCommand[0].validateGameCommand()
+    }
+
+    private fun Char.validateGameCommand() =
+        require(this == GAME_COMMAND_RETRY.value || this == GAME_COMMAND_QUIT.value) { "하나의 문자만 입력해 주세요." }
 
     companion object {
         private val BRIDGE_RANGE = 3..20
