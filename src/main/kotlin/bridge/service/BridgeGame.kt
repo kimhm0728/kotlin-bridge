b7805ed473a8faf2accbd6236f9395ba5532c9ce
kimhm0728 <kimhm0728@naver.com>
2023-12-13 18:07:53 +0900

    feat: 이동 결과를 객체에 저장하고 이동 가능 여부를 판별하는 기능 구현

--- src/main/kotlin/bridge/service/BridgeGame.kt
+++ src/main/kotlin/bridge/service/BridgeGame.kt
@@ -1,13 +1,34 @@
 package bridge.service
 
 import bridge.constants.Constants.GAME_COMMAND_RETRY
-import bridge.io.OutputView
+import bridge.constants.Constants.MOVING_UP
 import bridge.io.input.InputView
+import bridge.model.MovingResult
 import bridge.util.retryWhileNoException
 
-class BridgeGame {
+class BridgeGame(private val upResult: MovingResult, private val downResult: MovingResult) {
 
     private val inputView = InputView()
+
+    private fun moveResult(bridgeShape: String, moving: String): Boolean {
+        if (moving == MOVING_UP.value) {
+            downResult.add(" ")
+            if (bridgeShape != moving) {
+                upResult.add("X")
+                return false
+            }
+            upResult.add("O")
+            return true
+        }
+
+        upResult.add(" ")
+        if (bridgeShape != moving) {
+            downResult.add("X")
+            return false
+        }
+        downResult.add("O")
+        return true
+    }
 
     fun move() = retryWhileNoException {
         inputView.readMoving()
