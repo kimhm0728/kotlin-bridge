91dbca52ffaae9834fca6e0fe010b399aebd5752
kimhm0728 <kimhm0728@naver.com>
2023-12-13 16:56:14 +0900

    feat: 이동할 칸 예외 처리하는 기능 구현

---
+++ src/main/kotlin/bridge/constants/Constants.kt
@@ -0,0 +1,6 @@
+package bridge.constants
+
+enum class Constants(val value: Char) {
+    MOVING_UP('U'),
+    MOVING_DOWN('D')
+}
--- src/main/kotlin/bridge/io/input/InputValidator.kt
+++ src/main/kotlin/bridge/io/input/InputValidator.kt
@@ -1,5 +1,8 @@
 package bridge.io.input
 
+import bridge.constants.Constants.MOVING_UP
+import bridge.constants.Constants.MOVING_DOWN
+
 class InputValidator {
 
     fun validateBridgeSize(bridgeSize: String) {
@@ -12,6 +15,17 @@ class InputValidator {
 
     private fun Int.validateBridgeRange() =
         require(this in BRIDGE_RANGE) { "3 이상 20 이하의 숫자만 입력해 주세요." }
+
+    fun validateMoving(moving: String) {
+        moving.validateLength()
+        moving[0].validateMoving()
+    }
+
+    private fun String.validateLength() =
+        require(length == 1) { "하나의 문자만 입력해 주세요." }
+
+    private fun Char.validateMoving() =
+        require(this == MOVING_UP.value || this == MOVING_DOWN.value) { "하나의 문자만 입력해 주세요." }
 
     companion object {
         private val BRIDGE_RANGE = 3..20
