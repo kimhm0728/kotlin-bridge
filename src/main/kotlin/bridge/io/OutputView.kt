77e251a24ae66ae52f9a7f62122c4029f5f65719
kimhm0728 <kimhm0728@naver.com>
2023-12-13 18:04:41 +0900

    feat: 게임 라운드 결과를 출력하는 기능 구현

--- src/main/kotlin/bridge/io/OutputView.kt
+++ src/main/kotlin/bridge/io/OutputView.kt
@@ -1,5 +1,7 @@
 package bridge.io
 
+import bridge.model.MovingResult
+
 class OutputView {
 
     fun printStart() {
@@ -12,7 +14,10 @@ class OutputView {
      *
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    fun printMap() {}
+    fun printMap(upResult: MovingResult, downResult: MovingResult) {
+        println(upResult)
+        println(downResult)
+    }
 
     /**
      * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
